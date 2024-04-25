dfd0b02155ea89daedd48d41eeb237f32f4f7076
David Gao <gao.ten@northeastern.edu>
2024-04-25 11:24:16 -0400

    add code folding setup

---
+++ nvim/lua/plugins/code-folding.lua
@@ -0,0 +1,65 @@
+return {
+  "kevinhwang91/nvim-ufo",
+  dependencies = {
+    "kevinhwang91/promise-async",
+  },
+  config = function()
+    handler = function(virtText, lnum, endLnum, width, truncate)
+      local newVirtText = {}
+      local suffix = (" 󰁂 %d "):format(endLnum - lnum)
+      local sufWidth = vim.fn.strdisplaywidth(suffix)
+      local targetWidth = width - sufWidth
+      local curWidth = 0
+      for _, chunk in ipairs(virtText) do
+        local chunkText = chunk[1]
+        local chunkWidth = vim.fn.strdisplaywidth(chunkText)
+        if targetWidth > curWidth + chunkWidth then
+          table.insert(newVirtText, chunk)
+        else
+          chunkText = truncate(chunkText, targetWidth - curWidth)
+          local hlGroup = chunk[2]
+          table.insert(newVirtText, { chunkText, hlGroup })
+          chunkWidth = vim.fn.strdisplaywidth(chunkText)
+          -- str width returned from truncate() may less than 2nd argument, need padding
+          if curWidth + chunkWidth < targetWidth then
+            suffix = suffix .. (" "):rep(targetWidth - curWidth - chunkWidth)
+          end
+          break
+        end
+        curWidth = curWidth + chunkWidth
+      end
+      table.insert(newVirtText, { suffix, "MoreMsg" })
+      return newVirtText
+    end
+    -- global handler
+    -- `handler` is the 2nd parameter of `setFoldVirtTextHandler`,
+    -- check out `./lua/ufo.lua` and search `setFoldVirtTextHandler` for detail.
+
+    vim.o.foldcolumn = "1" -- '0' is not bad
+    vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
+    vim.o.foldlevelstart = 99
+    vim.o.foldenable = true
+    vim.g.ufo_fold_virt_text_handler = handler
+
+    -- nvim lsp as LSP client
+    local capabilities = vim.lsp.protocol.make_client_capabilities()
+    capabilities.textDocument.foldingRange = {
+      dynamicRegistration = false,
+      lineFoldingOnly = true,
+    }
+    local language_servers = require("lspconfig").util.available_servers()
+    for _, ls in ipairs(language_servers) do
+      require("lspconfig")[ls].setup({
+        capabilities = capabilities,
+        -- you can add other fields for setting up lsp server in this table
+      })
+    end
+    require("ufo").setup()
+  end,
+  keys = {
+    {
+      "<leader>z",
+      desc = "code folding+",
+    },
+  },
+}
