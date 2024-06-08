26bcf6ec0d915ecbd4a1d1ebb0be6a7281d3d25d
David Gao <gao.ten@northeastern.edu>
2024-06-07 22:39:01 -0400

    Update telescope.lua

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,8 +1,79 @@
 return {
   {
+    "folke/which-key.nvim",
+    opts = {
+      defaults = {
+        ["<leader>r"] = { name = "rename+" },
+      },
+    },
+  },
+  {
     "nvim-telescope/telescope.nvim",
+    branch = "master",
     event = "VeryLazy",
+    config = function()
+      -- local actions = require("telescope.actions")
+      require("telescope").setup({
+        -- Default configuration for telescope goes here:
+        -- config_key = value,
+        -- defaults = {
+        --   mappings = {
+        --     i = {
+        --       ["C-f"] = actions.preview_scrolling_up(5),
+        --       ["C-k"] = actions.preview_scrolling_right(5),
+        --       ["<C-f>"] = actions.results_scrolling_left(5),
+        --       ["<C-k>"] = actions.results_scrolling_right(5),
+        --     },
+        --   },
+        -- },
+      })
+      -- Enable Telescope extensions if they are installed
+      pcall(require("telescope").load_extension, "fzf")
+      pcall(require("telescope").load_extension, "ui-select")
+    end,
     keys = {
+      {
+        "<leader>gI",
+        function()
+          require("telescope.builtin").lsp_implementations()
+        end,
+        desc = "goto implementation",
+      },
+      {
+        "<leader>gr",
+        function()
+          require("telescope.builtin").lsp_references()
+        end,
+        desc = "goto references",
+      },
+      {
+        "<leader>D",
+        function()
+          require("telescope.builtin").lsp_type_definitions()
+        end,
+        desc = "goto word type definition",
+      },
+      {
+        "<leader>fa",
+        function()
+          require("telescope.builtin").lsp_document_symbols()
+        end,
+        desc = "fz find symbols current buffer",
+      },
+      {
+        "<leader>fw",
+        function()
+          require("telescope.builtin").lsp_dynamic_workspace_symbols()
+        end,
+        desc = "workspace fzf symbols",
+      },
+      {
+        "<leader>rn",
+        function()
+          vim.lsp.buf.rename()
+        end,
+        desc = "rename symbol accross project", -- if lsp server supports it
+      },
       {
         "<leader>fs",
         -- grep file content to find files
