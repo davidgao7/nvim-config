c5b478f59de2733ae84bc92da1729523b188ac59
David Gao <gao.ten@northeastern.edu>
2024-06-30 18:59:42 -0400

    add extension:advance git search

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -26,19 +26,21 @@ return {
         --     },
         --   },
         -- },
+
         -- view media files
         extensions = {
           media_files = {
             -- filetypes whitelist
             -- defaults to {"png", "jpg", "mp4", "webm", "pdf"}
             filetypes = { "png", "jpg", "gif", "mp4", "webm", "pdf" },
-            find_cmd = "chafa", -- find command (defaults to `fd`)
+            find_cmd = "rg", -- find command (defaults to `fd`)
           },
         },
       })
       -- Enable Telescope extensions if they are installed
       pcall(require("telescope").load_extension, "fzf")
       pcall(require("telescope").load_extension, "ui-select")
+      pcall(require("telescope").load_extension, "media_files")
     end,
     keys = {
       {
@@ -153,6 +155,79 @@ return {
           require("telescope").extensions.file_browser.file_browser()
         end,
         desc = "file browser",
+      },
+    },
+  },
+  {
+    "aaronhallaert/advanced-git-search.nvim",
+    dependencies = {
+      --- See dependencies
+      "nvim-telescope/telescope.nvim",
+      -- to show diff splits and open commits in browser
+      "tpope/vim-fugitive",
+      -- to open commits in browser with fugitive
+      "tpope/vim-rhubarb",
+      -- optional: to replace the diff from fugitive with diffview.nvim
+      -- (fugitive is still needed to open in browser)
+      "sindrets/diffview.nvim",
+    },
+    cmd = { "AdvancedGitSearch" },
+    config = function()
+      -- optional: setup telescope before loading the extension
+      require("telescope").setup({
+        extensions = {
+          -- advanced git search setup
+          advanced_git_search = {
+            -- Browse command to open commits in browser. Default fugitive GBrowse.
+            -- {commit_hash} is the placeholder for the commit hash.
+            browse_command = "GBrowse {commit_hash}",
+            -- when {commit_hash} is not provided, the commit will be appended to the specified command seperated by a space
+            -- browse_command = "GBrowse",
+            -- => both will result in calling `:GBrowse commit`
+
+            -- fugitive or diffview
+            diff_plugin = "diffview",
+            -- customize git in previewer
+            -- e.g. flags such as { "--no-pager" }, or { "-c", "delta.side-by-side=false" }
+            git_flags = {},
+            -- customize git diff in previewer
+            -- e.g. flags such as { "--raw" }
+            git_diff_flags = {},
+            -- Show builtin git pickers when executing "show_custom_functions" or :AdvancedGitSearch
+            show_builtin_git_pickers = true,
+            entry_default_author_or_date = "author", -- one of "author" or "date"
+            keymaps = {
+              -- following keymaps can be overridden
+              toggle_date_author = "<C-w>", -- toggle between author and date showing on telescope
+              open_commit_in_browser = "<C-o>",
+              copy_commit_hash = "<C-y>",
+              show_entire_commit = "<C-e>",
+            },
+
+            -- Telescope layout setup
+            telescope_theme = {
+              function_name_1 = {
+                -- Theme options
+              },
+              function_name_2 = "dropdown",
+              -- e.g. realistic example
+              show_custom_functions = {
+                layout_config = { width = 0.4, height = 0.4 },
+              },
+            },
+          },
+        },
+      })
+
+      require("telescope").load_extension("advanced_git_search")
+    end,
+    keys = {
+      -- lets setup some features I might used often
+      -- search current file commit history
+      {
+        "<leader>gS",
+        "<cmd>Telescope advanced_git_search diff_commit_file<cr>",
+        desc = "search current files commit history",
       },
     },
   },
