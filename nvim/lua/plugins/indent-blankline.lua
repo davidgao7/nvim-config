7996465b8f028715d8442092a6ad45db5c441be1
David Gao <gao.ten@northeastern.edu>
2024-07-27 15:36:49 -0400

    Update indent-blankline.lua
    add highlight groups

--- nvim/lua/plugins/indent-blankline.lua
+++ nvim/lua/plugins/indent-blankline.lua
@@ -1,6 +1,7 @@
 return {
   {
     "lukas-reineke/indent-blankline.nvim",
+    cond = true, -- disable the plugin until it's fixed
     main = "ibl",
     opts = {},
     char = "",
@@ -11,38 +12,48 @@ return {
     show_trailing_blankline_indent = false,
     config = function()
       -- if you want to highlight scop , uncomment this
-      --  local highlight_colors = {
-      --    "RainbowRed",
-      --    "RainbowYellow",
-      --    "RainbowBlue",
-      --    "RainbowOrange",
-      --    "RainbowGreen",
-      --    "RainbowViolet",
-      --    "RainbowCyan",
-      --      }
-      --      local hooks = require("ibl.hooks")
-      --      -- create the highlight groups in the highlight setup hook, so they are reset
-      --      -- every time the colorscheme changes
-      --      hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
-      --        vim.api.nvim_set_hl(0, "RainbowRed", { fg = "#E06C75" })
-      --        vim.api.nvim_set_hl(0, "RainbowYellow", { fg = "#E5C07B" })
-      --        vim.api.nvim_set_hl(0, "RainbowBlue", { fg = "#61AFEF" })
-      --        vim.api.nvim_set_hl(0, "RainbowOrange", { fg = "#D19A66" })
-      --        vim.api.nvim_set_hl(0, "RainbowGreen", { fg = "#98C379" })
-      --        vim.api.nvim_set_hl(0, "RainbowViolet", { fg = "#C678DD" })
-      --        vim.api.nvim_set_hl(0, "RainbowCyan", { fg = "#56B6C2" })
-      --      end)
-      --
+      local highlight_colors = {
+        "RainbowRed",
+        "RainbowYellow",
+        "RainbowBlue",
+        "RainbowOrange",
+        "RainbowGreen",
+        "RainbowViolet",
+        "RainbowCyan",
+      }
+      local hooks = require("ibl.hooks")
+      -- create the highlight groups in the highlight setup hook, so they are reset
+      -- every time the colorscheme changes
+      hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
+        vim.api.nvim_set_hl(0, "RainbowRed", { fg = "#E06C75" })
+        vim.api.nvim_set_hl(0, "RainbowYellow", { fg = "#E5C07B" })
+        vim.api.nvim_set_hl(0, "RainbowBlue", { fg = "#61AFEF" })
+        vim.api.nvim_set_hl(0, "RainbowOrange", { fg = "#D19A66" })
+        vim.api.nvim_set_hl(0, "RainbowGreen", { fg = "#98C379" })
+        vim.api.nvim_set_hl(0, "RainbowViolet", { fg = "#C678DD" })
+        vim.api.nvim_set_hl(0, "RainbowCyan", { fg = "#56B6C2" })
+      end)
+
       require("ibl").setup({
-        --indent = {
-        --  char = "|",
-        --},
+        -- indent = {
+        --   -- char = "|",
+        --   highlight = highlight_colors,
+        -- },
         whitespace = {
           highlight = "IblWhitespace",
           remove_blankline_trail = false,
         },
-        scope = { enabled = false },
+        scope = { highlight = highlight_colors },
       })
+
+      hooks.register(hooks.type.SCOPE_HIGHLIGHT, hooks.builtin.scope_highlight_from_extmark)
     end,
   },
+
+  -- NOTE: EMERGEYCY BACKUP PLAN
+  -- {
+  --   "lukas-reineke/indent-blankline.nvim",
+  --   main = "ibl",
+  --   commit = "29be0919b91fb59eca9e90690d76014233392bef",
+  -- },
 }
