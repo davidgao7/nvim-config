985023d2d3ea3b62c376cd55ee1236723ac168e0
David Gao <gao.ten@northeastern.edu>
2024-07-27 15:30:01 -0400

    Update copilot.lua
    rm legacy command

--- nvim/lua/plugins/copilot.lua
+++ nvim/lua/plugins/copilot.lua
@@ -1,21 +1,13 @@
 return {
-  "zbirenbaum/copilot.lua",
-  keys = {
-    -- goto next / previous suggestion
-    { "<C-n>", require("copilot.suggestion").next(), desc = "next" },
-    { "<C-p>", require("copilot.suggestion").prev(), desc = "prev" },
-    { "<C-yw>", require("copilot.suggestion").accept_word(), "accept_word" },
-    { "<C-yl>", require("copilot.suggestion").accept_line(), "accept_line" },
-    { "<C-c>", require("copilot.suggestion").dismiss(), "dismiss" },
-  },
-  cmd = "Copilot",
-  build = ":Copilot auth",
-  opts = {
-    suggestion = { enabled = true },
-    panel = { enabled = true },
-    filetypes = {
-      markdown = true,
-      help = true,
-    },
-  },
+	"zbirenbaum/copilot.lua",
+	cmd = "Copilot",
+	build = ":Copilot auth",
+	opts = {
+		suggestion = { enabled = false },
+		panel = { enabled = false },
+		filetypes = {
+			markdown = true,
+			help = true,
+		},
+	},
 }
