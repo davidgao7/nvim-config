f870858f4f81403f31bf827f317c299bd4e7b1a0
David Gao <gao.ten@northeastern.edu>
2024-07-27 15:24:25 -0400

    Update options.lua

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -7,11 +7,17 @@ vim.opt.winbar = "%=%m %f"
 -- always toggle off the conceallevel and always show everything
 vim.opt.conceallevel = 0
 -- set vertical refernce line
-vim.opt.colorcolumn = "100"
+vim.opt.colorcolumn = "79"
 -- enable mouse mode
 vim.opt.mouse = "a"
 -- set size of an indent to 4 spaces
 vim.opt.shiftwidth = 4
+-- a tab is 4 spaces
+vim.opt.tabstop = 4
+-- pressing tab key will insert 4 spaces
+vim.opt.expandtab = true
+-- number of spaces inserted instead of a tab
+vim.opt.softtabstop = 4
 
 -- indent blankline configuration
 vim.opt.list = true
@@ -21,3 +27,12 @@ vim.opt.list = true
 --vim.cmd([[highlight IndentBlanklineIndent4 guifg=#56B6C2 gui=nocombine]])
 --vim.cmd([[highlight IndentBlanklineIndent5 guifg=#61AFEF gui=nocombine]])
 --vim.cmd([[highlight IndentBlanklineIndent6 guifg=#C678DD gui=nocombine]])
+
+-- enable mouse mode
+vim.opt.mouse = "a"
+
+-- smart indent
+-- vim.opt.smartindent = true
+
+-- sync with system clipboard
+vim.opt.clipboard = vim.env.SSH_TTY and "" or "unnamedplus" -- Sync with system clipboard
