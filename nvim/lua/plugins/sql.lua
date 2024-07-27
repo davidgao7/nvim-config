ab1f6c448b8abd9d487825c7700c43eec534ee1d
David Gao <gao.ten@northeastern.edu>
2024-07-27 15:41:01 -0400

    Create sql.lua for db and sql interactions

---
+++ nvim/lua/plugins/sql.lua
@@ -0,0 +1,6 @@
+return {
+  -- sql plugins setup
+  { "tpope/vim-dadbod" }, -- for sql interaction
+  { "kristijanhusak/vim-dadbod-completion" },
+  { "kristijanhusak/vim-dadbod-ui" }, -- connect to a database
+}
