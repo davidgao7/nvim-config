b76ca5cfb8dfd0269f00a0dbe3b82bd8d27c45b7
David Gao <gao.ten@northeastern.edu>
2024-11-09 20:10:27 -0500

    Update python.lua

--- nvim/lua/plugins/python.lua
+++ nvim/lua/plugins/python.lua
@@ -1,120 +1,152 @@
 return {
-  -- TODO: check following tools -> mypy types-requests types-docutils
-  {
-    "williamboman/mason.nvim",
-    opts = function(_, opts)
-      -- vim.list_extend(opts.ensure_installed, { "pyright", "black", "ruff-lsp", "ruff" })
-      vim.list_extend(opts.ensure_installed, {
-        "black",
-        "pyright",
-        "debugpy",
-      })
-    end,
-  },
+	-- TODO: check following tools -> mypy types-requests types-docutils
+	{
+		"williamboman/mason.nvim",
+		opts = function(_, opts)
+			-- vim.list_extend(opts.ensure_installed, { "pyright", "black", "ruff-lsp", "ruff" })
+			vim.list_extend(opts.ensure_installed, {
+				-- "black",
+				"ruff",
+				"pyright",
+				"debugpy",
+			})
+		end,
+	},
 
-  -- Add `python` debugger to mason DAP to auto-install
-  -- Not absolutely necessary to declare adapter in `ensure_installed`, since `mason-nvim-dap`
-  -- has `automatic-install = true` in LazyVim by default and it automatically installs adapters
-  -- that are are set up (via dap) but not yet installed. Might as well skip the lines below as
-  -- a whole.
+	-- Add `python` debugger to mason DAP to auto-install
+	-- Not absolutely necessary to declare adapter in `ensure_installed`, since `mason-nvim-dap`
+	-- has `automatic-install = true` in LazyVim by default and it automatically installs adapters
+	-- that are are set up (via dap) but not yet installed. Might as well skip the lines below as
+	-- a whole.
 
-  -- Add which-key namespace for Python debugging
-  {
-    "folke/which-key.nvim",
-    optional = true,
-    opts = {
-      defaults = {
-        ["<leader>dP"] = { name = "+Python" },
-      },
-    },
-  },
+	-- Add which-key namespace for Python debugging
+	{
+		"folke/which-key.nvim",
+		optional = true,
+		opts = {
+			defaults = {
+				["<leader>dP"] = { name = "+Python" },
+			},
+		},
+	},
 
-  -- Setup `neotest`
-  {
-    "nvim-neotest/neotest",
-    dependencies = {
-      "nvim-neotest/neotest-python",
-    },
-    config = function()
-      require("neotest").setup({
-        adapters = {
-          require("neotest-python")({
-            -- Extra arguments for nvim-dap configuration
-            -- See https://github.com/microsoft/debugpy/wiki/Debug-configuration-settings for values
-            dap = { justMyCode = false },
-            -- Command line arguments for runner
-            -- Can also be a function to return dynamic values
-            args = { "--log-level", "DEBUG" },
-            -- Runner to use. Will use pytest if available by default.
-            -- Can be a function to return dynamic value.
-            runner = "pytest",
-            -- Custom python path for the runner.
-            -- Can be a string or a list of strings.
-            -- Can also be a function to return dynamic value.
-            -- If not provided, the path will be inferred by checking for
-            -- virtual envs in the local directory and for Pipenev/Poetry configs
-            python = function()
-              return vim.fn.input("Python executable path: ex. /usr/bin/python3: ")
-            end, -- ask user python execuable path
-            -- Returns if a given file path is a test file.
-            -- NB: This function is called a lot so don't perform any heavy tasks within it.
-            is_test_file = function(file_path)
-              return file_path:match("test_.*%.py$") ~= nil
-            end,
-            -- !!EXPERIMENTAL!! Enable shelling out to `pytest` to discover test
-            -- instances for files containing a parametrize mark (default: false)
-            pytest_discover_instances = false,
-          }),
-        },
-      })
-    end,
-  },
+	-- Add `server` and setup lspconfig
+	{
+		"neovim/nvim-lspconfig",
+		dependencies = {},
+		opts = {
+			servers = {
+				pyright = {
+					python = {
+						analysis = {
+							autoSearchPaths = true,
+							diagnosticMode = "workspace",
+							useLibraryCodeForTypes = true,
+						},
+					},
+				},
+			},
+			setup = {
+				pyright = function()
+					require("lazyvim.util").lsp.on_attach(function(client, _)
+						if client.name == "pyright" then
+							-- disable hover in favor of jedi-language-server(if you have)
+							client.server_capabilities.hoverProvider = true
+						end
+					end)
+				end,
+			},
+		},
+	},
+	{
+		"linux-cultist/venv-selector.nvim",
+		branch = "regexp", -- Use this branch for the new version
+		cmd = "VenvSelect",
+		enabled = function()
+			return LazyVim.has("telescope.nvim")
+		end,
+		opts = {
+			settings = {
+				options = {
+					notify_user_on_venv_activation = true,
+				},
+			},
+		},
+		--  Call config for python files and load the cached venv automatically
+		ft = "python",
+		keys = { { "<leader>cv", "<cmd>:VenvSelect<cr>", desc = "Select VirtualEnv", ft = "python" } },
+	},
 
-  -- Add `server` and setup lspconfig
-  {
-    "neovim/nvim-lspconfig",
-    dependencies = {},
-    opts = {
-      servers = {
-        pyright = {
-          python = {
-            analysis = {
-              autoSearchPaths = true,
-              diagnosticMode = "workspace",
-              useLibraryCodeForTypes = true,
-            },
-          },
-        },
-      },
-      setup = {
-        pyright = function()
-          require("lazyvim.util").lsp.on_attach(function(client, _)
-            if client.name == "pyright" then
-              -- disable hover in favor of jedi-language-server
-              client.server_capabilities.hoverProvider = false
-            end
-          end)
-        end,
-      },
-    },
-  },
+	-- python snippets
+	{
+		"garymjr/nvim-snippets",
+		opts = {
+			friendly_snippets = true,
+		},
+		dependencies = { "rafamadriz/friendly-snippets" },
+	},
 
-  -- Setup null-ls with `black`
-  -- {
-  --   "nvimtools/none-ls.nvim",
-  --   opts = function(_, opts)
-  --     local nls = require("null-ls")
-  --     opts.sources = vim.list_extend(opts.sources, {
-  --       -- Order of formatters matters. They are used in order of appearance.
-  --       nls.builtins.formatting.ruff,
-  --       nls.builtins.formatting.black,
-  --       -- nls.builtins.formatting.black.with({
-  --       --   extra_args = { "--preview" },
-  --       -- }),
-  --       -- nls.builtins.diagnostics.ruff,
-  --     })
-  --   end,
-  -- },
+	-- setup nvim-cmp snippets activate
+	{
+		"nvim-cmp",
+		optional = true,
+		dependencies = {
+			{
+				"garymjr/nvim-snippets",
+				opts = {
+					friendly_snippets = true,
+				},
+				dependencies = { "rafamadriz/friendly-snippets" },
+			},
+		},
+		opts = function(_, opts)
+			opts.snippet = {
+				expand = function(item)
+					return LazyVim.cmp.expand(item.body)
+				end,
+			}
+			if LazyVim.has("nvim-snippets") then
+				table.insert(opts.sources, { name = "snippets" })
+			end
+		end,
+		keys = {
+			{
+				"<Tab>",
+				function()
+					return vim.snippet.active({ direction = 1 }) and "<cmd>lua vim.snippet.jump(1)<cr>" or "<Tab>"
+				end,
+				expr = true,
+				silent = true,
+				mode = { "i", "s" },
+			},
+			{
+				"<S-Tab>",
+				function()
+					return vim.snippet.active({ direction = -1 }) and "<cmd>lua vim.snippet.jump(-1)<cr>" or "<S-Tab>"
+				end,
+				expr = true,
+				silent = true,
+				mode = { "i", "s" },
+			},
+		},
+	},
 
-  -- For selecting virtual envs
+	-- Setup null-ls with `black`
+	-- {
+	--   "nvimtools/none-ls.nvim",
+	--   opts = function(_, opts)
+	--     local nls = require("null-ls")
+	--     opts.sources = vim.list_extend(opts.sources, {
+	--       -- Order of formatters matters. They are used in order of appearance.
+	--       nls.builtins.formatting.ruff,
+	--       nls.builtins.formatting.black,
+	--       -- nls.builtins.formatting.black.with({
+	--       --   extra_args = { "--preview" },
+	--       -- }),
+	--       -- nls.builtins.diagnostics.ruff,
+	--     })
+	--   end,
+	-- },
+
+	-- For selecting virtual envs
 }
