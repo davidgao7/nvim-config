e62cdd59f145a58a391316fbd0f566f0fd811325
David Gao <gao.ten@northeastern.edu>
2024-11-09 20:13:22 -0500

    Update sql.lua

--- nvim/lua/plugins/sql.lua
+++ nvim/lua/plugins/sql.lua
@@ -1,6 +1,19 @@
 return {
-  -- sql plugins setup
-  { "tpope/vim-dadbod" }, -- for sql interaction
-  { "kristijanhusak/vim-dadbod-completion" },
-  { "kristijanhusak/vim-dadbod-ui" }, -- connect to a database
+	-- sql plugins setup
+	"kristijanhusak/vim-dadbod-ui", -- connect to a database
+
+	dependencies = {
+		{ "tpope/vim-dadbod", lazy = true }, -- for sql interaction
+		{ "kristijanhusak/vim-dadbod-completion", ft = { "sql", "mysql", "plsql" }, lazy = true }, -- Optional
+	},
+	cmd = {
+		"DBUI",
+		"DBUIToggle",
+		"DBUIAddConnection",
+		"DBUIFindBuffer",
+	},
+	init = function()
+		-- Your DBUI configuration
+		vim.g.db_ui_use_nerd_fonts = 1
+	end,
 }
